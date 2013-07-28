d47a419be2bfdf57a80d399d56147e8de3de435a
mubaidr <mubaidr@gmail.com>
2013-07-28 05:13:28 +0500

    v2
    -Added new navigation icons
    -Bug: Discover content does not load

--- js/start.js
+++ js/start.js
@@ -6,11 +6,53 @@ chrome.storage.sync.get(settings, function (e) {
 			elem.className += ' ' + settings[i];
 			if (settings[i] == "update-nav") {
 				var cont = document.getElementsByClassName('wrapper')[0];
-				var back = document.createElement('img');
-				var next = document.createElement('img');
-				back.setAttribute('id', 'nav_back');
-				back.setAttribute('src', '../img/icon_48.png');
-				cont.appendChild(back);
+				var view = document.getElementById('view-container');
+				cont.innerHTML = '';
+				var left = document.createElement('img');
+				var right = document.createElement('img');
+				left.setAttribute('id', 'nav_left');
+				right.setAttribute('id', 'nav_right');
+				left.setAttribute('tabindex', '1');
+				right.setAttribute('tabindex', '2');
+				left.setAttribute('class', 'nav_icons');
+				right.setAttribute('class', 'nav_icons');
+				left.setAttribute('src', chrome.runtime.getURL('img/left.png'));
+				right.setAttribute('src', chrome.runtime.getURL('img/right.png'));
+				left.addEventListener('click', function () {
+					var cl = view.className;
+					switch (cl) {
+						case 'hundred':
+							cl = cl.replace('hundred', 'zero');
+							break;
+						case 'two-hundred':
+							cl = cl.replace('two-hundred', 'hundred');
+							break;
+						default:
+							cl = 'zero';
+							break;
+					}
+					view.className = cl;
+					console.log(view.className);
+				})
+				right.addEventListener('click', function () {
+					var cl = view.className;
+					switch (cl) {
+						case 'zero':
+							cl = cl.replace('zero', 'hundred');
+							break;
+						case 'hundred':
+							cl = cl.replace('hundred', 'two-hundred');
+
+							break;
+						default:
+							cl = 'hundred';
+							break;
+					}
+					view.className = cl;
+					console.log(view.className);
+				})
+				cont.appendChild(left);
+				cont.appendChild(right);
 			}
 		}
 	}
